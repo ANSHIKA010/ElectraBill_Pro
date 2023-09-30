7c425c4f0136895aed0ab4dfbd081628d07c2c1d
ANSHIKA010 <sanshikas010@gmail.com>
2023-10-01 03:00:15 +0530

    Splash done

--- src/com/attech/electrabillpro/Splash.java
+++ src/com/attech/electrabillpro/Splash.java
@@ -1,4 +1,48 @@
 package com.attech.electrabillpro;
 
-public class Splash {
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame implements Runnable{
+
+    Thread thread2;
+    Splash(){
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("icons/electrabill_splash.jpeg"));
+        Image image = imageIcon.getImage().getScaledInstance(750, 500, Image.SCALE_DEFAULT);
+        JLabel jLabel = new JLabel(new ImageIcon(image));
+        add(jLabel);
+        setVisible(true);
+
+
+        for(int i=0,j=0; i<=750&&j<=500 ; i+=3, j+=2){
+            setSize(i, j);
+            setLocation(650-i/3, 400-j/2);
+            try {
+                Thread.sleep(5);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+
+        thread2 = new Thread(this);
+        thread2.start();
+
+    }
+
+    @Override
+    public void run() {
+        try{
+            Thread.sleep(5000);
+            setVisible(false);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        new Splash();
+    }
+
+
 }
